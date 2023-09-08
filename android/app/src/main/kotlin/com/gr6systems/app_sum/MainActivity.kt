2cfa0f0354b10f1718bc462479abc808bd552d48
Kelvin Guerra <grkelvin17@gmail.com>
2023-09-08 13:18:34 -0500

    version 1 de suma de dos numeros en Flutter

---
+++ android/app/src/main/kotlin/com/gr6systems/app_sum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gr6systems.app_sum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/suma.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SumaPage extends StatefulWidget {
+  const SumaPage({super.key});
+
+  @override
+  State<SumaPage> createState() => _SumaPageState();
+}
+
+class _SumaPageState extends State<SumaPage> {
+  TextEditingController num1 = TextEditingController();
+  TextEditingController num2 = TextEditingController();
+  int resultado = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SUMA"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 50.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: num1,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Número 1'),
+            ),
+            TextField(
+              controller: num2,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Número 2'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                int numero1 = int.tryParse(num1.text) ?? 0;
+                int numero2 = int.tryParse(num2.text) ?? 0;
+                int resul = numero1 + numero2;
+                setState(() {
+                  resultado = resul;
+                });
+              },
+              child: const Text('Calcular'),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              'Resultado: $resultado',
+              style:
+                  const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
