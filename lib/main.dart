ba98b14637bb21ceb76783df04d380e97c644bed
Kelvin Guerra <grkelvin17@gmail.com>
2023-09-10 10:56:05 -0500

    agregando comentarios a main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  /// La función de compilación devuelve un widget MaterialApp con el título 'Suma',
+  /// debugShowCheckedModeBanner configurado en falso y la página de inicio configurada en SumaPage.
+  /// 
+  /// Args:
+  ///   context (BuildContext): El parámetro de contexto es una referencia al contexto de compilación
+  /// actual. Proporciona acceso a diversa información y servicios relacionados con la compilación
+  /// actual, como el tema, la localización y la navegación. Normalmente se utiliza para acceder a
+  /// recursos y widgets dentro del árbol de widgets.
+  /// 
+  /// Returns:
+  ///   El método `build` devuelve un widget `MaterialApp`.
   Widget build(BuildContext context) {
     return const MaterialApp(
         title: 'Suma', debugShowCheckedModeBanner: false, home: SumaPage());
